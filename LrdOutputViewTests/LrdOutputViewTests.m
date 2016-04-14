c1cc404c1a08449b646cf9298bf9f84073ef34d0
SmallMonster77 <lrdupupup@gmail.com>
2016-04-14 13:47:23 +0800

    Initial Commit

---
+++ LrdOutputViewTests/LrdOutputViewTests.m
@@ -0,0 +1,39 @@
+//
+//  LrdOutputViewTests.m
+//  LrdOutputViewTests
+//
+//  Created by 键盘上的舞者 on 4/14/16.
+//  Copyright © 2016 键盘上的舞者. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LrdOutputViewTests : XCTestCase
+
+@end
+
+@implementation LrdOutputViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
